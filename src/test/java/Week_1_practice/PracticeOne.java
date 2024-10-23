d446a9fb7faa1db86777316cc9ef009c49095e9f
Qais <Qais_zaher@icloud.com>
2024-10-22 23:56:23 -0700

    practice

---
+++ src/test/java/Week_1_practice/PracticeOne.java
@@ -0,0 +1,28 @@
+package Week_1_practice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PracticeOne {
+    public static void main(String[] args) throws InterruptedException{
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://retail.tekschool-students.com/");
+
+        driver.manage().window().maximize();
+
+        Thread.sleep(2000);
+        //Step 1 Locate the Element
+        By signInLink = By.id("signinLink");
+
+        //Step 2 Ask Selenium to Find the Element.
+        WebElement signInElement = driver.findElement(signInLink);
+
+        //Step 3 Perform the Action.
+        signInElement.click();
+
+        Thread.sleep(10_000);
+
+        driver.quit();
+    }
+}
